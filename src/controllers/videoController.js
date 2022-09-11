b06566d714cfb6acf1d7a62c8b85e07d61dbad11
ellajang <3lla.jang@gmail.com>
2022-09-11 21:08:50 +0900

    first_wetube

---
+++ src/controllers/videoController.js
@@ -0,0 +1,46 @@
+import Video from "../models/Video";
+
+    // Video.find({},(error,videos) => {
+        
+    // });
+
+export const home = async(req,res) => {
+    const videos = await Video.find({});
+    return res.render("home.pug",{pageTitle:"Home", videos});
+}
+
+export const watch = (req,res) =>{
+    const {id} =req.params;
+    res.render("watch",{pageTitle:`Watching`});
+};
+export const getEdit = (req,res) =>{
+    const {id} =req.params;
+    return res.render("edit", {pageTitle:`Editing`});
+};
+export const postEdit = (req,res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req,res) => {
+    return res.render("upload",{pageTitle:"Upload Video"});
+};
+
+export const postUpload = async (req,res) => {
+    const { title,description,hashtags } = req.body;
+    try{
+        await Video.create({
+            title:title,
+            description:description,
+            hashtags:hashtags.split(",").map((word)=>`#${word}`),
+        });
+        return res.redirect("/");
+    }catch(error){
+        return res.render("upload",{
+            pageTitle:"Upload Video",
+            errorMessage:error._message,
+        });
+    }
+}
+
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController"; 
+
+const videoRouther = express.Router();
+
+videoRouther.get("/:id(\\d+)",watch);
+videoRouther.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouther.route("/upload").get(getUpload).post(postUpload);
+export default videoRouther;
