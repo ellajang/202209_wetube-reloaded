85587ce6678fab6a07b0337a1e2b8c7032ca33ed
ellajang <3lla.jang@gmail.com>
2022-09-15 19:14:09 +0900

    fourth_wetube

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController"; 
 import { protectorMiddleware, videoUpload } from "../middlewares";
+import Video from "../models/Video";
 
 const videoRouther = express.Router();
 
@@ -9,3 +10,4 @@ videoRouther.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEd
 videoRouther.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo);
 videoRouther.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.single("video"),postUpload);
 export default videoRouther;
+
